3840b503ba7c37043fd9a2ebbaf4d85dfda5d026
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-11 17:04:15 +0300

    add security config classes (#2)

---
+++ src/main/java/com/greenblat/vktesttask/security/UserDetailsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.greenblat.vktesttask.security;
+
+import com.greenblat.vktesttask.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new UserDetailsImpl(user);
+    }
+}
