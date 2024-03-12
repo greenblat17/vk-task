6410f25b2ee370384e794d092fc900fe1f5e339a
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-12 20:21:36 +0300

    feat: implement caching (#12)

--- src/main/java/com/greenblat/vktesttask/service/PostService.java
+++ src/main/java/com/greenblat/vktesttask/service/PostService.java
@@ -3,6 +3,9 @@ package com.greenblat.vktesttask.service;
 import com.greenblat.vktesttask.dto.PostDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -26,6 +29,7 @@ public class PostService {
                 .block();
     }
 
+    @Cacheable(value = "PostService::loadPostById", key = "#id")
     public PostDto loadPostById(Long id) {
         log.info("log posts with id={}", id);
         return webClient.get()
@@ -35,6 +39,7 @@ public class PostService {
                 .block();
     }
 
+    @Cacheable(value = "PostService::loadPostById", key = "#postDto.id()")
     public PostDto savePost(PostDto postDto) {
         return webClient.post()
                 .uri(String.join("", "/posts"))
@@ -44,6 +49,7 @@ public class PostService {
                 .block();
     }
 
+    @CachePut(value = "PostService::loadPostById", key = "#id" )
     public PostDto updatePost(Long id, PostDto postDto) {
         return webClient.put()
                 .uri(String.join("", "/posts/", id.toString()))
@@ -53,6 +59,7 @@ public class PostService {
                 .block();
     }
 
+    @CacheEvict(value = "PostService::loadPostById", key = "#id")
     public void delete(Long id) {
         webClient.delete()
                 .uri(String.join("", "/posts/", id.toString()));
