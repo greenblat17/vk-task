227597a635cbe1c45f805e66197704bab8ee54ae
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-12 08:14:28 +0300

    feat: implement refreshing token (#6)

--- src/main/java/com/greenblat/vktesttask/service/AuthService.java
+++ src/main/java/com/greenblat/vktesttask/service/AuthService.java
@@ -1,8 +1,10 @@
 package com.greenblat.vktesttask.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.greenblat.vktesttask.dto.auth.AuthRequest;
 import com.greenblat.vktesttask.dto.auth.AuthResponse;
 import com.greenblat.vktesttask.dto.auth.RegisterRequest;
+import com.greenblat.vktesttask.exception.ObjectResponseException;
 import com.greenblat.vktesttask.exception.ResourceNotFoundException;
 import com.greenblat.vktesttask.mapper.UserMapper;
 import com.greenblat.vktesttask.model.Token;
@@ -12,11 +14,19 @@ import com.greenblat.vktesttask.repository.TokenRepository;
 import com.greenblat.vktesttask.repository.UserRepository;
 import com.greenblat.vktesttask.security.JwtService;
 import com.greenblat.vktesttask.security.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
@@ -31,13 +41,15 @@ public class AuthService {
 
     public AuthResponse registerUser(RegisterRequest request) {
         User user = userMapper.mapToUser(request, passwordEncoder.encode(request.password()));
-
         var savedUser = userRepository.save(user);
-        var authResponse = buildAuthResponseWithToken(savedUser);
 
-        saveUserToken(savedUser, authResponse.token());
+        var userDetails = convertToUserDetails(user);
+        var jwtToken = jwtService.generateToken(userDetails);
+        var refreshToken = jwtService.generateRefreshToken(userDetails);
 
-        return authResponse;
+        saveUserToken(savedUser, jwtToken);
+
+        return new AuthResponse(jwtToken, refreshToken);
     }
 
     public AuthResponse authenticateUser(AuthRequest request) {
@@ -46,14 +58,55 @@ public class AuthService {
                 request.password()
         );
         authenticationManager.authenticate(authInputToken);
-
         var user = getUser(request.username());
-        var authResponse = buildAuthResponseWithToken(user);
+
+        var userDetails = convertToUserDetails(user);
+        var jwtToken = jwtService.generateRefreshToken(userDetails);
+        var refreshToken = jwtService.generateRefreshToken(userDetails);
 
         revokeAllUserTokens(user);
-        saveUserToken(user, authResponse.token());
+        saveUserToken(user, jwtToken);
 
-        return authResponse;
+        return new AuthResponse(jwtToken, refreshToken);
+    }
+
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) {
+        final String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+        final String refreshToken;
+        final String username;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return;
+        }
+
+        refreshToken = authHeader.substring(7);
+        username = jwtService.extractUsername(refreshToken);
+
+        if (username != null) {
+            var user = getUser(username);
+            var userDetails = convertToUserDetails(user);
+            if (jwtService.isRefreshTokenValid(refreshToken, userDetails)) {
+                var accessToken = jwtService.generateToken(userDetails);
+
+                revokeAllUserTokens(user);
+                saveUserToken(user, accessToken);
+
+                var authResponse = new AuthResponse(accessToken, refreshToken);
+                writeToResponse(response, authResponse);
+            }
+        }
+    }
+
+    private void writeToResponse(HttpServletResponse response, AuthResponse authResponse) {
+        try {
+            new ObjectMapper().writeValue(response.getOutputStream(), authResponse);
+        } catch (IOException e) {
+            throw new ObjectResponseException(e.getMessage());
+        }
+    }
+
+    private UserDetails convertToUserDetails(User user) {
+        return new UserDetailsImpl(user);
     }
 
     private User getUser(String username) {
@@ -63,12 +116,6 @@ public class AuthService {
                                 "User with username [%s] not found"
                                         .formatted(username)
                         ));
-    }
-
-    private AuthResponse buildAuthResponseWithToken(User user) {
-        var userDetails = new UserDetailsImpl(user);
-        var jwtToken = jwtService.generateToken(userDetails);
-        return new AuthResponse(jwtToken);
     }
 
     private void revokeAllUserTokens(User user) {
