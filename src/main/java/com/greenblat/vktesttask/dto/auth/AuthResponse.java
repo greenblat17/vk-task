81a5a3027071a6cacc4ec3344a64153966f34b3f
Alex Zhuravlev <leftemail17@mail.ru>
2024-03-12 08:13:15 +0300

    ref: add refresh token to dto (#6)

--- src/main/java/com/greenblat/vktesttask/dto/auth/AuthResponse.java
+++ src/main/java/com/greenblat/vktesttask/dto/auth/AuthResponse.java
@@ -1,4 +1,9 @@
 package com.greenblat.vktesttask.dto.auth;
 
-public record AuthResponse(String token) {
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+
+@Builder
+public record AuthResponse(@JsonProperty("access_token") String accessToken,
+                           @JsonProperty("refresh_token") String refreshToken) {
 }
